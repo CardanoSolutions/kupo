f5fc50d2045022c4e8a50d1a0d03e0ed37753ee6
KtorZ <matthias.benkort@gmail.com>
2022-08-03 16:38:06 +0200

    Fix memory-leak in (direct) chain-sync client.
      This appeared after the introduction of the forced-rollback command. I
      don't know what's causing the leak per se, but inlining the argument
      is fixing it. Somehow.
    
      I'll try to borrow Marcin's brain on that, he might have a better
      explanation than me on what's going on here.

--- src/Kupo/App/ChainSync/Direct.hs
+++ src/Kupo/App/ChainSync/Direct.hs
@@ -42,18 +42,28 @@ mkChainSyncClient
     -> Mailbox m (Tip, block) (Tip, Point)
     -> [Point]
     -> ChainSyncClientPipelined block Point Tip m ()
-mkChainSyncClient forcedRollbackVar mailbox pts =
+mkChainSyncClient var0 mailbox pts =
     ChainSyncClientPipelined $ pure $
-        SendMsgFindIntersect pts (clientStIntersect Nothing)
+        -- /!\ NOTE:
+        -- We inline and embed 'var0' with each sub-function call here instead
+        -- of using it from the function context because this otherwise creates
+        -- a massive memory-leak. Why? I don't quite know, and it is still
+        -- puzzling me. It seems that GHC keeps allocating partially applied
+        -- functions that do not get garbage-collected somehow. As a results,
+        -- the heap just keep growing. Inlining the TMVar nicely fixes the
+        -- problem.
+        -- Interestingly, there's no issue with the 'mailbox'.
+        SendMsgFindIntersect pts (clientStIntersect var0 Nothing)
   where
     clientStIntersect
-        :: Maybe (Point, ForcedRollbackHandler m)
+        :: TMVar m (Point, ForcedRollbackHandler m)
+        -> Maybe (Point, ForcedRollbackHandler m)
         -> ClientPipelinedStIntersect block Point Tip m ()
-    clientStIntersect forcedRollback = ClientPipelinedStIntersect
+    clientStIntersect var forcedRollback = ClientPipelinedStIntersect
         { recvMsgIntersectFound = \_point _tip -> do
             whenJust forcedRollback $ \(_, ForcedRollbackHandler{onSuccess}) ->
                 onSuccess
-            clientStIdle Zero
+            clientStIdle var Zero
         , recvMsgIntersectNotFound = \(getTipSlotNo -> tip) -> do
             case forcedRollback of
                 Just (pointSlot -> point, ForcedRollbackHandler{onFailure}) -> do
@@ -67,16 +77,17 @@ mkChainSyncClient forcedRollbackVar mailbox pts =
 
     clientStIdle
         :: forall n. ()
-        => Nat n
+        => TMVar m (Point, ForcedRollbackHandler m)
+        -> Nat n
         -> m (ClientPipelinedStIdle n block Point Tip m ())
-    clientStIdle n = do
-        atomically (tryTakeTMVar forcedRollbackVar) >>= \case
+    clientStIdle var n = do
+        atomically (tryTakeTMVar var) >>= \case
             Nothing ->
                 pure $ SendMsgRequestNextPipelined $ CollectResponse
-                    (guard (natToInt n < maxInFlight) $> clientStIdle (Succ n))
-                    (clientStNext n)
+                    (guard (natToInt n < maxInFlight) $> clientStIdle var (Succ n))
+                    (clientStNext var n)
             Just (pt, handler) ->
-                pure (clientStWrapUp (pt, handler) n)
+                pure (clientStCollect var (pt, handler) n)
 
     -- | When receiving a 'forcedRollback' request, we simply collect and drop
     -- all messages in flight and look for the requested intersection. Note
@@ -95,35 +106,37 @@ mkChainSyncClient forcedRollbackVar mailbox pts =
     -- checkpoints, as normal. This means that in the worse case scenario, we
     -- interrupt the chain-sync client for a short bit and we continue back
     -- where we were at.
-    clientStWrapUp
+    clientStCollect
         :: forall n. ()
-        => (Point, ForcedRollbackHandler m)
+        => TMVar m (Point, ForcedRollbackHandler m)
+        -> (Point, ForcedRollbackHandler m)
         -> Nat n
         -> ClientPipelinedStIdle n block Point Tip m ()
-    clientStWrapUp (pt, handler) = \case
+    clientStCollect var (pt, handler) = \case
         Zero ->
-            SendMsgFindIntersect [pt] (clientStIntersect (Just (pt, handler)))
+            SendMsgFindIntersect [pt] (clientStIntersect var (Just (pt, handler)))
         Succ n ->
             CollectResponse
                 Nothing
                 ( ClientStNext
                     { recvMsgRollForward = \_block _tip -> do
-                        pure $ clientStWrapUp (pt, handler) n
+                        pure $ clientStCollect var (pt, handler) n
                     , recvMsgRollBackward = \_point _tip -> do
-                        pure $ clientStWrapUp (pt, handler) n
+                        pure $ clientStCollect var (pt, handler) n
                     }
                 )
 
     clientStNext
         :: forall n. ()
-        => Nat n
+        => TMVar m (Point, ForcedRollbackHandler m)
+        -> Nat n
         -> ClientStNext n block Point Tip m ()
-    clientStNext n =
+    clientStNext var n =
         ClientStNext
             { recvMsgRollForward = \block tip -> do
                 atomically (putHighFrequencyMessage mailbox (tip, block))
-                clientStIdle n
+                clientStIdle var n
             , recvMsgRollBackward = \point tip -> do
                 atomically (putIntermittentMessage mailbox (tip, point))
-                clientStIdle n
+                clientStIdle var n
             }
