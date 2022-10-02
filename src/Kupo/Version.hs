29088566ea4d93c53ebde6993ebd60c90a58464f
KtorZ <matthias.benkort@gmail.com>
2022-10-02 16:47:17 +0200

    Append git sha to version when available.
      This should help debugging versions used by people when they run an a nightly version of Kupo.

--- src/Kupo/Version.hs
+++ src/Kupo/Version.hs
@@ -2,6 +2,8 @@
 --  License, v. 2.0. If a copy of the MPL was not distributed with this
 --  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+{-# LANGUAGE TemplateHaskell #-}
+
 module Kupo.Version
     ( version
     ) where
@@ -11,8 +13,12 @@ import Kupo.Prelude
 import Data.Version
     ( showVersion
     )
+import Kupo.Version.TH
+    ( gitRevisionTH
+    )
 
 import qualified Paths_kupo as Pkg
 
 version :: Text
-version = toText ("v" <> showVersion Pkg.version)
+version =
+    toText ("v" <> showVersion Pkg.version <> $(gitRevisionTH))
---
+++ src/Kupo/Version/TH.hs
@@ -0,0 +1,31 @@
+--  This Source Code Form is subject to the terms of the Mozilla Public
+--  License, v. 2.0. If a copy of the MPL was not distributed with this
+--  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+module Kupo.Version.TH where
+
+import Kupo.Prelude
+
+import Language.Haskell.TH
+    ( Exp (..)
+    , Lit (..)
+    , Q
+    , runIO
+    )
+import System.Environment
+    ( lookupEnv
+    )
+
+-- | Dynamically lookup a git revision in the environment as `GIT_SHA` at compile-time to enhance
+-- the version number.
+gitRevisionTH :: Q Exp
+gitRevisionTH = do
+    LitE . StringL <$> runIO lookupGitSha
+  where
+    lookupGitSha :: IO String
+    lookupGitSha = do
+        lookupEnv "GIT_SHA" <&> \case
+            Nothing ->
+                ""
+            Just sha ->
+                "+" <> take 6 sha
