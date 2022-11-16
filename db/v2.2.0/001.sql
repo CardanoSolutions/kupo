03a3ba1574c278c0cc84358df3498db84964c76c
KtorZ <matthias.benkort@gmail.com>
2022-11-16 12:22:43 +0100

    Simplify the migration for v2.2.0

--- db/v2.2.0/001.sql
+++ db/v2.2.0/001.sql
@@ -1,9 +1,4 @@
 DELETE FROM checkpoints WHERE slot_no >= (SELECT MIN(created_at) FROM inputs WHERE datum_hash IS NOT NULL);
-DELETE FROM inputs      WHERE created_at > (SELECT MIN(created_at) FROM inputs WHERE datum_hash IS NOT NULL);
-CREATE INDEX IF NOT EXISTS inputsBySpentAt ON inputs(spent_at);
-UPDATE inputs SET spent_at = NULL WHERE spent_at >= (SELECT MIN(created_at) FROM inputs WHERE datum_hash IS NOT NULL);
-DROP INDEX inputsBySpentAt;
-DELETE FROM inputs WHERE datum_hash IS NOT NULL;
-
+DROP INDEX IF EXISTS inputsByDatumHash;
 ALTER TABLE inputs RENAME COLUMN datum_hash TO datum_info;
 ALTER TABLE inputs ADD COLUMN datum_hash BLOB GENERATED ALWAYS AS (substr(datum_info, 2)) VIRTUAL;
