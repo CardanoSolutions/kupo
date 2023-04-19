5fa1065478eb57cfa4bf2b12816e5be3655d6da8
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-04-19 10:58:02 +0200

    Connect Hydra ChainSync to mailbox with hard-coded data

--- src/Kupo/App/ChainSync/Hydra.hs
+++ src/Kupo/App/ChainSync/Hydra.hs
@@ -2,6 +2,7 @@
 --  License, v. 2.0. If a copy of the MPL was not distributed with this
 --  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+{-# LANGUAGE PatternSynonyms #-}
 module Kupo.App.ChainSync.Hydra
     ( connect
     , runChainSyncClient
@@ -34,9 +35,22 @@ import Kupo.Data.Ogmios
     ( PartialBlock
     )
 
+import qualified Data.ByteString as BS
+import Kupo.Data.Cardano.HeaderHash
+    ( unsafeHeaderHashFromBytes
+    )
+import Kupo.Data.Cardano.Point
+    ( pattern BlockPoint
+    )
+import Kupo.Data.Cardano.Tip
+    ( pattern Tip
+    )
 import Kupo.Data.Hydra
     ( HydraMessage (..)
     , decodeHydraMessage
+    )
+import Kupo.Data.PartialBlock
+    ( PartialBlock (..)
     )
 import qualified Network.WebSockets as WS
 import qualified Network.WebSockets.Json as WS
@@ -56,7 +70,17 @@ runChainSyncClient mailbox beforeMainLoop pts ws = do
     beforeMainLoop
     forever $ do
         WS.receiveJson ws decodeHydraMessage >>= \case
-            SnapshotConfirmed{} -> pure ()
+            SnapshotConfirmed{} -> do
+                let headerHash = unsafeHeaderHashFromBytes $ BS.replicate 32 0
+                let slotNo = 1 -- TODO: decode snapshot number as slot
+                let blockNo = 1
+                let tip = Tip slotNo headerHash blockNo
+                let block =
+                        PartialBlock
+                            { blockPoint = BlockPoint slotNo headerHash
+                            , blockBody  = []
+                            }
+                atomically (putHighFrequencyMessage mailbox (tip, block))
             SomethingElse -> pure ()
 
 connect
