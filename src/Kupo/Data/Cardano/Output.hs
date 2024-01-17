142a33c04cf8bae43deeb74de50d73ee04966645
klarkc <walkerleite490@gmail.com>
2024-01-17 11:39:14 -0300

    Fix production errors

--- src/Kupo/Data/Cardano/Output.hs
+++ src/Kupo/Data/Cardano/Output.hs
@@ -41,7 +41,6 @@ import qualified Cardano.Ledger.Address as Ledger
 import qualified Cardano.Ledger.Alonzo.TxBody as Ledger.Alonzo
 import qualified Cardano.Ledger.Babbage.TxBody as Ledger.Babbage
 import qualified Cardano.Ledger.Coin as Ledger
-import qualified Cardano.Ledger.Conway as Ledger.Conway
 import qualified Cardano.Ledger.Core as Ledger.Core
 import qualified Cardano.Ledger.Mary.Value as Ledger
 import qualified Cardano.Ledger.Shelley.Tx as Ledger.Shelley
