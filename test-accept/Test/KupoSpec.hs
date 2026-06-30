4d7cd1a87314caa422364b6c6640c1a268f9ccc5
notunrandom <122674938+notunrandom@users.noreply.github.com>
2026-06-30 12:57:21 +0000

    Extract 'can connect' test to new test-accept

--- test-accept/Test/KupoSpec.hs
+++ test-accept/Test/KupoSpec.hs
@@ -23,16 +23,23 @@ import Control.Exception
     , bracket
     , try
     )
+import Data.Char
+    (ord
+    )
 import Network.HTTP.Client
     ( HttpException
+    , Request
     , defaultManagerSettings
-    , httpNoBody
+    , httpLbs
     , newManager
     , parseRequest
+    , responseBody
     , responseStatus
     )
 import Network.HTTP.Types.Status
-    ( status202
+    ( Status
+    , status202
+    , status200
     )
 import System.Directory
     ( Permissions (..)
@@ -47,7 +54,8 @@ import System.IO.Temp
     ( createTempDirectory
     )
 import System.Process
-    ( proc
+    ( CreateProcess
+    , proc
     , withCreateProcess
     )
 import Test.Hspec
@@ -58,31 +66,64 @@ import Test.Hspec
     , shouldReturn
     )
 
+import qualified Data.ByteString.Lazy as B
+
+type ResponseCheck = Status -> B.ByteString -> Bool
+
 spec :: Spec
-spec = around withDir $ do
-    describe "Kupo server start/restart corner cases" $ do
-        it "Can start in readonly mode on readonly DB" $ \dir -> do
-            createDatabaseFiles dir
-            makeUnwritable dir
-            startReadOnly dir `shouldReturn` True
+spec = do
+
+    describe "Kupo server start/restart corner cases (in-memory DB)" $ do
+
+        it "Can connect" $ do
+            startInMemorySinceOriginMatchShelley `shouldReturn` True
+
+    around withDir $ do
+
+        describe "Kupo server start/restart corner cases (file DB)" $ do
+
+            it "Can start in readonly mode on readonly DB" $ \dir -> do
+                createDatabaseFiles dir
+                makeUnwritable dir
+                startReadOnly dir `shouldReturn` True
+
+startInMemorySinceOriginMatchShelley :: IO Bool
+startInMemorySinceOriginMatchShelley = do
+    process <- kupo
+        ["--since"      , "origin"
+        ,"--match"      , "*/*"
+        ,"--in-memory"
+        ]
+    withCreateProcess process $ \ _ _ _ _ -> do
+        eventually isConnected
 
 withDir :: (FilePath -> IO ()) -> IO ()
 withDir = bracket (createTempDirectory "." "test-tmp") removePathForcibly
 
 createDatabaseFiles :: FilePath -> IO ()
 createDatabaseFiles dir = do
+    process <- kupo
+        ["--since"  , "tip"
+        ,"--match"  , "*/*"
+        ,"--workdir", dir
+        ]
+    withCreateProcess process $ \ _ _ _ _ -> do
+        ready <- eventually isReady
+        pure (assert ready ())
+
+kupo :: [String] -> IO CreateProcess
+kupo options = do
     socket <- getEnv "CARDANO_NODE_SOCKET"
     config <- getEnv "CARDANO_NODE_CONFIG"
-    let kupo = proc "kupo"
-            ["--node-socket", socket
-            ,"--node-config", config
-            ,"--workdir",     dir
-            ,"--since",       "tip"
-            ,"--match",       "*/*"
-            ]
-    withCreateProcess kupo $ \ _ _ _ _ -> do
-        ready <- eventuallyReady
-        pure (assert ready ())
+    pure $ proc "kupo"
+        (["--node-socket", socket
+         ,"--node-config", config
+         ]
+         ++ options
+        )
+
+makeUnwritable :: FilePath -> IO ()
+makeUnwritable = setWritable False
 
 startReadOnly :: FilePath -> IO Bool
 startReadOnly dir = do
@@ -91,24 +132,21 @@ startReadOnly dir = do
             ,"--workdir", dir
             ]
     withCreateProcess kupo $ \ _ _ _ _ -> do
-        eventuallyReady
-
-makeUnwritable :: FilePath -> IO ()
-makeUnwritable = setWritable False
+        eventually isReady
 
 setWritable :: Bool -> FilePath -> IO ()
 setWritable x path = do
     p <- getPermissions path
     setPermissions path (p {writable = x})
 
-eventuallyReady :: IO Bool
-eventuallyReady = go (0::Int)
+eventually :: IO Bool -> IO Bool
+eventually p = go (0::Int)
     where
         go attempt
             | attempt > 20 = pure False
             | otherwise    = check attempt
         check attempt = do
-            ready <- tryHttp isReady
+            ready <- tryHttp p
             case ready of
                 Right True -> pure True
                 _          -> wait attempt
@@ -116,12 +154,43 @@ eventuallyReady = go (0::Int)
             threadDelay 1_000_000  -- 1s between retries
             go (attempt + 1)
 
+isConnected :: IO Bool
+isConnected = checkResponse "/checkpoints" containsNonEmptyList
+
 isReady :: IO Bool
-isReady = do
-  manager <- newManager defaultManagerSettings
-  request <- parseRequest (url <> "/health")
-  response <- httpNoBody request manager
-  pure (responseStatus response == status202)
+isReady = checkResponse "/health" isHealthy
+
+checkResponse :: String -> ResponseCheck -> IO Bool
+checkResponse path check = do
+    manager  <- newManager defaultManagerSettings
+    request' <- request path
+    response <- httpLbs request' manager
+    let
+        status = responseStatus response
+        body   = responseBody   response
+    pure (check status body)
+
+containsNonEmptyList :: Status -> B.ByteString -> Bool
+containsNonEmptyList status body =
+    status == status200
+    && B.length body > 2
+    && startsWithBracket body
+    && endsWithBracket   body
+
+startsWithBracket :: B.ByteString -> Bool
+startsWithBracket xs =
+    case B.uncons xs of
+        Nothing     -> False
+        Just (w, _) -> w == fromIntegral (ord '[')
+
+endsWithBracket :: B.ByteString -> Bool
+endsWithBracket xs =
+    case B.unsnoc xs of
+        Nothing     -> False
+        Just (_, w) -> w == fromIntegral (ord ']')
+
+isHealthy :: ResponseCheck
+isHealthy status _ = status == status202
 
 tryHttp :: IO a -> IO (Either HttpException a)
 tryHttp m = try m
@@ -129,4 +198,5 @@ tryHttp m = try m
 url :: String
 url = "http://127.0.0.1:1442"
 
-
+request :: String -> IO Request
+request = parseRequest . (url <>)
